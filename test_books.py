02375cc0a564fce8f93974827dbd6d962877733c
ch1ckenrice <panasyuk2007@gmail.com>
2026-05-20 16:51:54 +0200

    Add test for default book status

--- test_books.py
+++ test_books.py
@@ -159,3 +159,12 @@ class TestBooksFilter:
 
         assert response.status_code == 200
         assert response.get_json() == []
+def test_create_book_default_status(self, client):
+    """Книга створюється зі статусом за замовчуванням"""
+
+    response = client.post("/api/books", json={
+        "title": "Test Book",
+        "created_by": "Дмитро Панасюк",
+    })
+
+    assert response.status_code == 201
